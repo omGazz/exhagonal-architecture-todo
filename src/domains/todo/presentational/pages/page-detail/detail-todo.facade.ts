4adeda6b739ae2cc96a4a0cbd021bdbf60ac95e5
omGazz <omgazz89@gmail.com>
2024-04-18 12:16:34 +0200

    clean code

--- src/domains/todo/presentational/pages/page-detail/detail-todo.facade.ts
+++ src/domains/todo/presentational/pages/page-detail/detail-todo.facade.ts
@@ -1,21 +1,5 @@
-import { Injectable, computed, inject, signal } from '@angular/core';
-import { toDo } from '../../../models/to-do.model';
-import { Loading } from '../../../../../core/utils/loading.decorator';
-import { TodoPort } from '../../../ports/todo.port';
-import { ListService } from '../../../services/list.service';
+import { Injectable, computed, inject } from '@angular/core';
 import { DetailService } from '../../../services/detail.service';
-
-//Facade or service?
-
-//Facade is a design pattern that provides a simplified interface to a larger body of code, such as a class library.
-// service is a class with a focused purpose, such as handling the business logic of an application.
-
-//It could be perfectly fine use for example 2 differents services, one to handle details logic and another to handle list logic
-
-/* ************************************************************ 
-Switch to the branch "feature/facade-per-page" to see the implementation of a Facade per page
-IMPORTANT: We can create a Facade for each pages or for each domain, it depends on the complexity of the application, in this case we are using a Facade for the domain
-************************************************************ */
 
 @Injectable({
   providedIn: 'root',
